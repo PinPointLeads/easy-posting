cfe3439fe42ce78a484913757d637af1fad882d2
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2026-02-05 16:30:37 +0100

    dashboard and protected fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="w-full max-w-lg mx-auto px-4 py-8">
+    <div className="w-full max-w-lg mx-auto px-4 py-8 ">
       <h1 className="text-2xl font-bold mb-6">Create Post</h1>
       <PostCreator />
     </div>
